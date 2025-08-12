426a260d99b0e08f37198a038d557ba7aeb266ee
imanuelsammeta <40435449+imanuelsammeta@users.noreply.github.com>
2025-08-12 08:48:25 -0500

    Update prep.py

--- data-science/src/prep.py
+++ data-science/src/prep.py
@@ -16,7 +16,7 @@ def parse_args():
     '''Parse input arguments'''
 
     parser = argparse.ArgumentParser("prep")  # Create an ArgumentParser object
-    parser.add_argument("--data", type=str, help="Path to raw data")  # Specify the type for raw data (str)
+    parser.add_argument("--raw_data", type=str, help="Path to raw data")  # Specify the type for raw data (str)
     parser.add_argument("--train_data", type=str, help="Path to train dataset")  # Specify the type for train data (str)
     parser.add_argument("--test_data", type=str, help="Path to test dataset")  # Specify the type for test data (str)
     parser.add_argument("--test_train_ratio", type=float, default=0.2, help="Test-train ratio")  # Specify the type (float) and default value (0.2) for test-train ratio
@@ -28,7 +28,7 @@ def main(args):  # Write the function name for the main data preparation logic
     '''Read, preprocess, split, and save datasets'''
 
     # Reading Data
-    df = pd.read_csv(args.data)
+    df = pd.read_csv(args.raw_data)
 
     # Encode categorical feature
     le = LabelEncoder()
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     args = parser.parse_args()  # Call the function to parse arguments
 
     lines = [
-        f"Raw data path: {args.data}",  # Print the data path
+        f"Raw data path: {args.raw_data}",  # Print the data path
         f"Train dataset output path: {args.train_data}",  # Print the train_data path
         f"Test dataset path: {args.test_data}",  # Print the test_data path
         f"Test-train ratio: {args.test_train_ratio}",  # Print the test_train_ratio
